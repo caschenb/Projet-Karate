96ce12c783090e028188eb9a64df2220b979a3eb
caschenb <celine.aschen@gmail.com>
2015-05-20 18:46:58 +0200

    Update competition_modif.php
    on ne peut entrer un materiau que pour les competitions correspondantes,
    si on change d'une compet avec à une compet sans le materiau est supprimé

--- competition_modif.php
+++ competition_modif.php
@@ -11,30 +11,60 @@ $querystring = "SELECT * FROM projet_karate.competition WHERE  projet_karate.com
 		$query = pg_query($idConnexion, $querystring);
 		$result=pg_fetch_array($query);
 ?>
+<script type="text/javascript">
+      function catsel(sel) {
+        //if (sel.value=="-1" ) return;
+        var opt=sel.getElementsByTagName("option" );
+        for (var i=0; i<opt.length; i++) {
+          var x=document.getElementById(opt[i].value);
+          if (x) x.style.display="none";
+        }
+        var cat = document.getElementById(sel.value);
+        if (cat) cat.style.display="block";
+      }
+    </script>
+	
 	<form method = "GET" action="competition_modif_res.php">
 	<p> Remplissez les champs que vous souhaitez modifier </p>
 				<p><label for="nom">Nom :</label><br />
 				<?php 
 				echo"
-				<input type = 'text' name = 'nom' value=$result[nom]  id='nom'></p>
-				<p><label for='date'>Date :</label><br />
-				<input type = 'text' name = 'date' value=$result[date] id='date'></p>
-				<p><label for='lieu'>Lieu :</label><br />
-				<input type = 'text' name = 'lieu' value=$result[lieu] id='lieu'></p>
-				<p><label for='site'>Site Web :</label><br />
-				<input type = 'text' name = 'site' value=$result[siteweb] id='site'></p>
-				<p><label for='mail'>Mail :</label><br />
+				<input type='hidden' name='anciennom'  value=$nom > 
+				<input type = 'text' name = 'nom' value=$result[nom]  ></p>
+				<p>Date :<br />
+				<input type = 'text' name = 'date' value=$result[date]></p>
+				<p>Lieu :<br />
+				<input type = 'text' name = 'lieu' value=$result[lieu] ></p>
+				<p>Site Web :<br />
+				<input type = 'text' name = 'site' value=$result[siteweb] ></p>
+				<p>Mail :<br />
 				<input type = 'text' name = 'mail' value=$result[mail] ></p>
-				<p><label for='materiau'>Nom Du Materiau :</label><br />
-				<input type = 'text' name = 'materiau' value=$result[nommateriau] ></p>
-				<p><label for='type'>Nom de la Competition</label><br />
-				<input type='text' name='type' value=$result[type] id='type'> </p> 
-				<input type='hidden' name='anciennom' value=$nom> "; ?>
+				<p> Ancien type de Competition: $result[type] </p> 
+				<p>Nom de la Competition<br />";
+				?>
+				<select name="type" onchange="catsel(this)">
+					<option value="CompetitionKata">Kata</option>
+				    <option value="CompetitionKumite">Kumite</option>
+				    <option value="CompetitionTameshiWari">Tameshi Wari</option>
+				    <option value="CompetitionMixte">Mixte</option>
+				</select> <br/>
+				<div id="CompetitionTameshiWari" style="display:none">
+					<p>Nom Du Materiau :<br />
+					<input type = 'text' name = 'materiau' <?php echo" value=$result[nommateriau] "; ?>></p>
+				</div>
+				<div id="CompetitionMixte" style="display:none">
+					<p>Nom Du Materiau :<br />
+					<input type = 'text' name = 'materiau' <?php echo" value=$result[nommateriau] "; ?>></p>
+				</div>
+				<div id="CompetitionKata" style="display:none">      <!-- si le type de competition choisie est kata ou kumite le type de materiau est mis a NUL !-->
+					<input type='hidden' name='materiau'  value='' > 	
+				</div>
+				<div id="CompetitionKumite" style="display:none">
+					<input type='hidden' name='materiau'  value='' > 	
+				</div>
 				<input type = "submit">
 				</form>
-
-
-
+		
 
 <?php
 pg_close($idConnexion); //on ferme la connexion à notre base de donnée
